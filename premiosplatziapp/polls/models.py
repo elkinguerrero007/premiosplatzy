100cca70eee331c6bcb9bfebb75cafcf5d54179f
elkin <3439@holbertonschool.com>
2022-06-25 12:44:54 -0500

    quitando las s

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-class choises(models.Model):
+class choise(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choise_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
