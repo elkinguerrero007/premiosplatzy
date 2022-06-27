9f5dcd1951db0cf13c38443d8956900015cbf911
elkin <3439@holbertonschool.com>
2022-06-27 16:43:34 -0500

    cuadrando las views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    return HttpResponse("Estas en la pagina principa de premios platzy")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def result(request, question_id):
+    return HttpResponse(f"Estas viendo los resultado de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando la pregunta numero {question_id}")
+
+
+
